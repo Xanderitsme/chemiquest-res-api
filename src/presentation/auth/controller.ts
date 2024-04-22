b581c8a71efb73cd58367ab45bceeb15be8cd13a
DiegoAlessandroMotta <diegomottaq@gmail.com>
2024-04-22 17:14:51 -0500

    Implement use RegisterUserDto

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,7 +1,14 @@
 import { type Request, type Response } from 'express'
+import { RegisterUserDto } from '../../domain/dtos'
 
 export class AuthController {
-  static registerUser = (_req: Request, res: Response) => {
-    res.status(200).json({ message: 'Register user' })
+  static registerUser = (req: Request, res: Response) => {
+    const [error, user] = RegisterUserDto.create(req.body)
+
+    if (error !== undefined) {
+      res.status(404).json({ error })
+    }
+
+    res.status(200).json(user)
   }
 }
