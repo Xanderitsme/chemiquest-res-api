471895c5d67bd5ec81d3c0c7ac013090fed4b631
Xanderitsme <xanderitsme@gmail.com>
2024-04-22 00:07:50 -0500

    Add project eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: 'love',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  }
+}
