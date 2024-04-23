756168bde2b84d2f21917102b79e7358f3d8eb24
Xanderitsme <xanderitsme@gmail.com>
2024-04-23 00:14:39 -0500

    Implements RegisterUser use case in AuthController

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,14 +1,35 @@
 import { type Request, type Response } from 'express'
 import { RegisterUserDto } from '../../domain/dtos'
+import { RegisterUser } from '../../domain/use-cases'
+import { type AuthRepository } from '../../domain/repositories'
+import { CustomError } from '../../domain/errors'
 
 export class AuthController {
-  static registerUser = (req: Request, res: Response) => {
-    const [error, user] = RegisterUserDto.create(req.body)
+  constructor (
+    private readonly authRepository: AuthRepository
+  ) {}
 
-    if (error !== undefined) {
-      res.status(404).json({ error })
+  private readonly handleError = (error: any, res: Response) => {
+    if (error instanceof CustomError) {
+      res.status(error.statusCode).json({ error: error.message })
+      return
     }
 
-    res.status(200).json(user)
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
+
+  registerUser = (req: Request, res: Response) => {
+    const [error, registerUserDto] = RegisterUserDto.create(req.body)
+
+    if (error !== undefined || registerUserDto === undefined) {
+      res.status(404).json({ error })
+      return
+    }
+
+    const registerUser = new RegisterUser(this.authRepository)
+
+    registerUser.execute(registerUserDto)
+      .then(user => res.status(200).json(user))
+      .catch(error => { this.handleError(error, res) })
   }
 }
--- src/presentation/auth/routes.ts
+++ src/presentation/auth/routes.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express'
 import { AuthController } from './controller'
+import { AuthRepositoryImpl } from '../../infrastructure/repositories/auth.repository.impl'
+import { AuthDatasourceImpl } from '../../infrastructure/datasources/auth.datasource.impl'
 
 export class AuthRoutes {
   static get routes () {
     const router = Router()
 
-    router.post('/register', AuthController.registerUser)
+    const authDatasource = new AuthDatasourceImpl()
+    const authRepository = new AuthRepositoryImpl(authDatasource)
+    const authController = new AuthController(authRepository)
+
+    router.post('/register', authController.registerUser)
 
     return router
   }
