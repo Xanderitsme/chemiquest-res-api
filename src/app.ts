8979924b7f0ddf518259715ac2366f57d06e66af
Xanderitsme <xanderitsme@gmail.com>
2024-04-23 00:32:23 -0500

    Implements MongoDatabase connection

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
+import { envs } from './config'
+import { MongoDatabase } from './data/mongodb'
 import { AppRoutes } from './presentation/routes'
 import { Server } from './presentation/server'
 
 const main = async () => {
+  await MongoDatabase.connect({
+    mongoUrl: envs.MONGO_URL,
+    dbName: envs.MONGO_DB_NAME
+  })
+
   const server = new Server({
+    port: envs.PORT,
     routes: AppRoutes.routes
   })
 
