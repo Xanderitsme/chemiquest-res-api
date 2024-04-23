e09abe04d35ba50b33a07bbf5da198f6387f6e18
Xanderitsme <xanderitsme@gmail.com>
2024-04-23 00:10:46 -0500

    Implement basic RegisterUser

--- src/domain/use-cases/auth/register-user.use-case.ts
+++ src/domain/use-cases/auth/register-user.use-case.ts
@@ -1,11 +1,18 @@
 import { type RegisterUserDto } from '../../dtos/auth/register-user.dto'
+import { type AuthRepository } from '../../repositories'
 
 interface RegisterUserUseCase {
   execute: (registerUserDto: RegisterUserDto) => Promise<any>
 }
 
 export class RegisterUser implements RegisterUserUseCase {
-  async execute () {
+  constructor (
+    private readonly authRepository: AuthRepository
+  ) {}
 
+  async execute (registerUserDto: RegisterUserDto) {
+    const user = await this.authRepository.register(registerUserDto)
+
+    return user
   }
 }
