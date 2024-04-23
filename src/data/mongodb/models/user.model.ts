4f097ee4d72eb09801f1367860dddee3473ff51c
DiegoAlessandroMotta <diegomottaq@gmail.com>
2024-04-22 22:22:23 -0500

    Add mongo user model

---
+++ src/data/mongodb/models/user.model.ts
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, 'username is required']
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, 'email is required']
+  },
+  password: {
+    type: String,
+    required: [true, 'password is required']
+  }
+})
+
+export const UserModel = mongoose.model('User', userSchema)
