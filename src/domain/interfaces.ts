919b4aa0a5d22f4ad303557155230f4860ef0616
DiegoAlessandroMotta <diegomottaq@gmail.com>
2024-04-23 20:00:30 -0500

    Add Encryptor definition

--- src/domain/interfaces.ts
+++ src/domain/interfaces.ts
@@ -8,3 +8,8 @@ export interface RegisterUserUseCase {
 export interface TokenAuthenticator {
   generateToken: (payload: object, duration?: string) => Promise<string | null>
 }
+
+export interface Encryptor {
+  hash: (password: string) => Promise<string>
+  compare: (password: string, hash: string) => Promise<boolean>
+}
