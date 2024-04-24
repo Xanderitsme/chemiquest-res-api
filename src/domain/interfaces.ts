1af633354ace2682cffcccbcac61b4f457a2dbb0
Xanderitsme <xanderitsme@gmail.com>
2024-04-24 08:53:57 -0500

    Add LoginUserUseCase definition

--- src/domain/interfaces.ts
+++ src/domain/interfaces.ts
@@ -1,9 +1,5 @@
-import { type RegisterUserDto } from './dtos'
+import { type LoginUserDto, type RegisterUserDto } from './dtos'
 import { type UserToken } from './types'
-
-export interface RegisterUserUseCase {
-  execute: (registerUserDto: RegisterUserDto) => Promise<UserToken>
-}
 
 export interface TokenAuthenticator {
   generateToken: (payload: object, duration?: string) => Promise<string | null>
@@ -12,4 +8,12 @@ export interface TokenAuthenticator {
 export interface Encryptor {
   hash: (password: string) => Promise<string>
   compare: (password: string, hash: string) => Promise<boolean>
+}
+
+export interface RegisterUserUseCase {
+  execute: (registerUserDto: RegisterUserDto) => Promise<UserToken>
+}
+
+export interface LoginUserUseCase {
+  execute: (loginUserDto: LoginUserDto) => Promise<UserToken>
 }
