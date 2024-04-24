780d2832473f9b592b03453d406174249e1d8949
Xanderitsme <xanderitsme@gmail.com>
2024-04-24 08:32:18 -0500

    Add login user dto

---
+++ src/domain/dtos/auth/login-user.dto.ts
@@ -0,0 +1,37 @@
+import { Validators } from '../../../config'
+
+export class LoginUserDto {
+  public usernameEmail: string
+  public password: string
+
+  private constructor (user: LoginUserDto) {
+    this.usernameEmail = user.usernameEmail
+    this.password = user.password
+  }
+
+  static create (input: any): [string?, LoginUserDto?] {
+    const {
+      username,
+      password
+    } = input
+
+    if (username === undefined ||
+      typeof username !== 'string' ||
+      (!Validators.username(username) &&
+      !Validators.email(username)) ||
+      password === undefined ||
+      typeof password !== 'string' ||
+      !Validators.password(password)
+    ) {
+      return ['Missing or incorrect username or password', undefined]
+    }
+
+    return [
+      undefined,
+      new LoginUserDto({
+        usernameEmail: username,
+        password
+      })
+    ]
+  }
+}
