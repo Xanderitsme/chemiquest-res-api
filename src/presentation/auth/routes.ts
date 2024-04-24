38b9452a668c7aa45bc267193979d028b1e1c728
DiegoAlessandroMotta <diegomottaq@gmail.com>
2024-04-23 19:14:53 -0500

    Implements TokenAuthenticator dependency injection in AuthRoutes

--- src/presentation/auth/routes.ts
+++ src/presentation/auth/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import { AuthController } from './controller'
 import { AuthDatasourceImpl } from '../../infrastructure/datasources'
 import { AuthRepositoryImpl } from '../../infrastructure/repositories'
+import { JwtAdapter } from '../../config'
 
 export class AuthRoutes {
   static get routes () {
@@ -9,7 +10,7 @@ export class AuthRoutes {
 
     const authDatasource = new AuthDatasourceImpl()
     const authRepository = new AuthRepositoryImpl(authDatasource)
-    const authController = new AuthController(authRepository)
+    const authController = new AuthController(authRepository, JwtAdapter)
 
     router.post('/register', authController.registerUser)
 
