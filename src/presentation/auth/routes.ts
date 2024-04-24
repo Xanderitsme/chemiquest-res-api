85908febeb6d14925d23ab4e011a64d83bab892e
DiegoAlessandroMotta <diegomottaq@gmail.com>
2024-04-23 20:04:26 -0500

    Implements Encryptor dependency injection in AuthRoutes

--- src/presentation/auth/routes.ts
+++ src/presentation/auth/routes.ts
@@ -2,13 +2,13 @@ import { Router } from 'express'
 import { AuthController } from './controller'
 import { AuthDatasourceImpl } from '../../infrastructure/datasources'
 import { AuthRepositoryImpl } from '../../infrastructure/repositories'
-import { JwtAdapter } from '../../config'
+import { BcryptAdapter, JwtAdapter } from '../../config'
 
 export class AuthRoutes {
   static get routes () {
     const router = Router()
 
-    const authDatasource = new AuthDatasourceImpl()
+    const authDatasource = new AuthDatasourceImpl(BcryptAdapter)
     const authRepository = new AuthRepositoryImpl(authDatasource)
     const authController = new AuthController(authRepository, JwtAdapter)
 
