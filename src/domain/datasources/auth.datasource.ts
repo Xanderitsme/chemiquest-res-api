870bdc42d560c23a80bf179f55c03c7c1e6c84ca
Xanderitsme <xanderitsme@gmail.com>
2024-04-24 08:34:26 -0500

    Add AuthDatasource login definition

--- src/domain/datasources/auth.datasource.ts
+++ src/domain/datasources/auth.datasource.ts
@@ -1,6 +1,7 @@
-import { type RegisterUserDto } from '../dtos'
+import { type LoginUserDto, type RegisterUserDto } from '../dtos'
 import { type UserEntity } from '../entities'
 
 export abstract class AuthDatasource {
   abstract register (registerUserDto: RegisterUserDto): Promise<UserEntity>
+  abstract login (loginUserDto: LoginUserDto): Promise<UserEntity>
 }
